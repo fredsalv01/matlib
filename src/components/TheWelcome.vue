4901042cc6270e49fc8c2eefdefdb719c49187c8
fredalbert37 <fredalbert37@outlook.com>
2022-08-25 19:48:21 -0500

    changes in formula boxes

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -33,50 +33,59 @@ export default {
 
 <template>
   <div>
-      <div
-        v-for="topic in topics"
-        :key="topic.id"
-        style="
-          border: 0.5px solid gray;
-          border-radius: 20px;
-          margin: 15px auto;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-            rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-          padding: 1rem 2rem;
-        "
-      >
-        <WelcomeItem>
-          <template #icon>
-            <SupportIcon />
-          </template>
-          <template #heading>Tema:</template>
-          {{ topic.name }}
-        </WelcomeItem>
+    <div
+      v-for="topic in topics"
+      :key="topic.id"
+      style="
+        border: 0.5px solid gray;
+        border-radius: 20px;
+        margin: 15px auto;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+          rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+        padding: 1rem 1rem;
+        width: 100%;
+      "
+    >
+      <WelcomeItem>
+        <template #icon>
+          <SupportIcon />
+        </template>
+        <template #heading>Tema:</template>
+        {{ topic.name }}
+      </WelcomeItem>
 
-        <WelcomeItem>
-          <template #icon>
-            <DocumentationIcon />
-          </template>
-          <template #heading>Descripcion:</template>
-          {{ topic.description }}
-        </WelcomeItem>
+      <WelcomeItem>
+        <template #icon>
+          <DocumentationIcon />
+        </template>
+        <template #heading>Descripcion:</template>
+        {{ topic.description }}
+      </WelcomeItem>
 
-        <WelcomeItem>
-          <template #icon>
-            <EcosystemIcon />
-          </template>
-          <template #heading>Ecuaciones</template>
-          <ol style="padding: 0">
-            <li
-              style="margin-top: 1.2rem; width: 50%"
-              v-for="(formula, index) in topic.formulas"
-              :key="index"
-              :index="index"
-            >
-              <vue-mathjax :formula="formula"></vue-mathjax>
-            </li>
-          </ol>
-        </WelcomeItem>
-      </div>
+      <WelcomeItem>
+        <template #icon>
+          <EcosystemIcon />
+        </template>
+        <template #heading>Ecuaciones</template>
+        <ol
+          style="
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: flex-start;
+          "
+        >
+          <li
+            style="margin-top: 1.2rem; padding-left: 5px;"
+            v-for="(formula, index) in topic.formulas"
+            :key="index"
+            :index="index"
+          >
+            <vue-mathjax :formula="formula"></vue-mathjax>
+          </li>
+        </ol>
+      </WelcomeItem>
+    </div>
   </div>
 </template>
